2ca80d81de8fd24cc84412bcd42dba6d2554290e
LauCaro15 <lccandamil@gmail.com>
2024-05-08 16:39:40 -0500

    Fix: URL Undefined and Style: Put Up Title

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -7,24 +7,6 @@ export const navItems: NavItem[] = [
   {
     title: "Quiénes somos",
     link: "/about",
-    /* subItems: [
-      {
-        title: "Proyecto Vive y Camina",
-        link: "https://adoratricesmalaga.es/proyecto-vive-y-camina",
-      },
-      {
-        title: "Nuestra historia",
-        link: "https://adoratricesmalaga.es/nuestra-historia",
-      },
-      {
-        title: "Propósito y Valores",
-        link: "https://adoratricesmalaga.es/mision-vision-y-valores",
-      },
-      {
-        title: "Política y estrategia de Calidad",
-        link: "https://adoratricesmalaga.es/politica-y-estrategia-de-calidad",
-      },
-    ], */
   },
   {
     title: "Qué hacemos",
@@ -41,59 +23,8 @@ export const navItems: NavItem[] = [
         title: "Trabajo en Red",
         link: "/trabajo-red",
       },
-      /* {
-        title: "Atención educativa",
-        link: "https://adoratricesmalaga.es/atencion-educativa",
-      },
-      {
-        title: "Atención sociolaboral",
-        link: "https://adoratricesmalaga.es/atencion-sociolaboral",
-      },
-      {
-        title: "Atención psicológica",
-        link: "https://adoratricesmalaga.es/atencion-psicologica",
-      },
-      {
-        title: "Atención sanitaria",
-        link: "https://adoratricesmalaga.es/atencion-sanitaria",
-      },
-      {
-        title: "Atención jurídica",
-        link: "https://adoratricesmalaga.es/atencion-juridica",
-      },
-      {
-        title: "Acogida Territorial",
-        link: "https://adoratricesmalaga.es/atencion-no-residencial",
-      }, */
     ],
   },
-  /* {
-    title: "Trabajo en red",
-    subItems: [
-      { title: "Redes", link: "https://adoratricesmalaga.es/redes" },
-      {
-        title: "Entidades colaboradoras y financiadoras",
-        link: "https://adoratricesmalaga.es/entidades-colaboradoras-y-financiadoras",
-      },
-    ],
-  }, */
-  /* {
-    title: "Trabajos",
-    subItems: [
-      {
-        title: "Trabajo intercongregacional",
-        link: "/trabajo-intercongregacional",
-      },
-      {
-        title: "Trabajo en red",
-        link: "/trabajo-red",
-      },
-    ],
-  }, */
-  /* {
-    title: "Caminando juntos",
-    link: "/caminando-juntos",
-  }, */
   {
     title: "Productos y Servicios",
     link: "/services/",
@@ -102,21 +33,8 @@ export const navItems: NavItem[] = [
     title: "Noticias y Actividades",
     link: "/posts",
   },
-  /* { title: "Materiales", link: "https://adoratricesmalaga.es/materiales" }, */
   {
     title: "Contacto",
     link: "/contact",
   },
-
-  /* {
-    title: "Súmate",
-    link: "https://adoratricesmalaga.es/sumate",
-    subItems: [
-      {
-        title: "Voluntariado",
-        link: "https://adoratricesmalaga.es/voluntariado",
-      },
-    ],
-  },
-  { title: "DONAR", link: "https://adoratricesmalaga.es/colabora" }, */
 ];
